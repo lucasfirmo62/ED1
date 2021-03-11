39d9f63accd91177819764bfa24a72fa17c46e52
lucasfirmo62 <58527718+lucasfirmo62@users.noreply.github.com>
2021-03-10 16:49:08 -0800

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void vogais(int* qtd, int tam, char* string){
+
+    int count = 0;
+
+    for(int i = 0; i < tam; i++){
+        if(string[i] == '\0'){
+            break;
+        }
+        else if(string[i] == 'a' || string[i] == 'A' ||
+            string[i] == 'e' || string[i] == 'E' ||
+            string[i] == 'i' || string[i] == 'I' ||
+            string[i] == 'o' || string[i] == 'O' ||
+            string[i] == 'u' || string[i] == 'U'){
+            count++;
+        }
+    }
+    
+    *qtd = count;
+
+}
+
+void print(int* qtd){
+    printf("%d\n", *qtd);
+}
+
+
+int main(){
+
+    char frase[] = {"Eu estudo na UTFPR"};
+    int tamanho = 30;
+    int quantidade = 0;
+
+    vogais(&quantidade, tamanho, frase);
+    print(&quantidade);
+
+}
