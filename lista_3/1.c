efbe7d2392dcde4075b2c1d7eb3ab3ba276b5ff9
lucasfirmo62 <58527718+lucasfirmo62@users.noreply.github.com>
2021-03-17 17:49:59 -0300

    Add files via upload

---
+++ lista_3/1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print(int* vetor, int tam){
+    printf("[");
+    for(int i = 0; i < tam; i++){
+        printf("%d", vetor[i]);
+        if(i != tam-1) printf(", ");
+    }
+    printf("]\n");
+}
+
+int main(){
+    int* v = (int*) calloc(5, sizeof(int));
+    *(v) = 1;
+    *(v+1) = 2;
+    *(v+2) = 3;
+    *(v+3) = 4;
+    *(v+4) = 5;
+
+    print(v, 5);
+
+    *(v) = -10;
+    *(v+1) = 20;
+    *(v+2) = 283;
+    *(v+3) = 19;
+    *(v+4) = -05;
+
+    print(v, 5);
+
+    free(v);
+
+}
