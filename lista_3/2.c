a26c5bfb544912c52332710b92e450234898fdf3
lucasfirmo62 <58527718+lucasfirmo62@users.noreply.github.com>
2021-03-18 20:04:38 -0300

    Add files via upload

--- lista_3/2.c
+++ lista_3/2.c
@@ -2,22 +2,6 @@
 #include <stdlib.h>
 
 
-void preencher_p(int* vetor, int tamanho){
-     *vetor = (int*) calloc(tamanho, sizeof(int));
-    for(int i = 0; i < tamanho; i++){
-        vetor[i] = tamanho*(i);
-    }
-    
-}
-
-int* preencher(int* vetor, int tamanho){
-    vetor = (int*) calloc(tamanho, sizeof(int));
-    for(int i = 0; i < tamanho; i++){
-        vetor[i] = tamanho*(i);
-    }
-    return vetor;
-}
-
 void print(int* vetor, int tam){
     printf("[");
     for(int i = 0; i < tam; i++){
@@ -27,15 +11,31 @@ void print(int* vetor, int tam){
     printf("]\n");
 }
 
+void preencher_p(int** vetor, int tamanho){
+    *vetor = (int*) calloc(tamanho, sizeof(int));
+    for(int i = 0; i < tamanho; i++){
+        *(*vetor+i) = tamanho*(i);
+    }
+    
+}
+
+int* preencher(int tamanho){
+    int* vetor = (int*) calloc(tamanho, sizeof(int));
+    for(int i = 0; i < tamanho; i++){
+        *(vetor+i) = tamanho*(i);
+    }
+    return vetor;
+}
+
 int main(){
 
     int tam = 5;
     int* v1;
     int* v2;
 
-    preencher_p(v1, tam);
+    preencher_p(&v1, tam);
     print(v1, tam);
-    v2 = preencher(v2, tam);
+    v2 = preencher(tam);
     print(v2, tam);
 
     free(v1);
